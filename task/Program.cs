17ca37cbf428e2676a064a9c8829a2a1a705fa9c
akhmed <akhmed@storyAkhmedMacBook-Air.local>
2022-12-08 03:07:50 +0300

    Решение задачи

---
+++ task/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] array1 = new string[5] {"1234", "233", "hello", "world", "mma"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length < 4)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array1);
+Console.WriteLine();
+Console.Write("Вывод массива длина которых меньше или равна 3: ");
+PrintArray(array2);
